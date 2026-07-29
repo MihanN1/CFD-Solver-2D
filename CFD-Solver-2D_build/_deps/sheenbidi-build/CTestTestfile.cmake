5dc1e7cbf2e0b34d056ced49ae1383ba646f32ea
Kuzya-kotav <105818672+Kuzya-kotav@users.noreply.github.com>
2026-07-29 20:41:34 +0300

    Cmake
    Cmake Cuda would shoulda do some more

---
+++ CFD-Solver-2D_build/_deps/sheenbidi-build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/alans/3D Objects/.git/CFD-Solver-2D_build/_deps/sheenbidi-src
+# Build directory: C:/Users/alans/3D Objects/.git/CFD-Solver-2D_build/_deps/sheenbidi-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
